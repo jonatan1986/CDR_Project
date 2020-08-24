1001c0f9a5a3baa32ac3f814d3ba2795ce32d836
jonatan1986 <buga1986@gmail.com>
2020-08-24 16:06:47 +0300

    Update SharedResource.h

--- SharedResource.h
+++ SharedResource.h
@@ -1,5 +1,5 @@
 /*
- * ThreadArgs.h
+ * SharedResource.h
  *
  *  Created on: Aug 11, 2020
  *      Author: yonathan
@@ -15,6 +15,17 @@
 #include "Config.h"
 #include "SingleTone.h"
 
+/*
+this  struct is a shared resouce between the threads eWriter,eReader and eParser.
+@ m_parseQueueMutex - locks the access of the parser, till  m_queueToParse is not empty
+@ m_writeQueueMutex - locks the access of the writer, till  m_queueToWrite is not empty
+@ m_bExitParseThread - boolean var which indicates that parser should finish it's job once it emptied all
+element from m_queueToParse. initialized to false 
+@ m_bExitWriteThread - boolean var which indicates that eWriter should finish it's job once it emptied all
+element from m_queueToWrite. initialized to false
+@ m_queueToParse - shared queue among eReader and eParser. eReader inserts, eParser removes.
+@ m_queueToWrite - shared queue among eWriter and eParser. eParser inserts, eWriter removes.
+*/
 struct SharedResource
 {
     SharedResource():m_bExitParseThread(false),m_bExitWriteThread(false)
@@ -29,7 +40,12 @@ struct SharedResource
     SafeQueue<std::string> m_queueToParse;
     SafeQueue<eCdrDetails> m_queueToWrite;
 };
-
+/*
+this class wraps "SharedResource". since the shared resources should have global access it should be wrapped with singleTone.
+however there is more then one thread which has sub-threads(eReader,eWriter,eParser) so one instance of SharedResource will
+not be a good solution(SingleTone provides only one instance),thus SharedResourceWrapper contains an array of "SharedResource"
+so "SharedResourceWrapper" is wrapped by SingleTone - and each group of subthreads share an element of the array(of type "SharedResource").
+*/
 class SharedResourceWrapper
 {
 public:
